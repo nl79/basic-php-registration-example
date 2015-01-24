29a4279117af3649d65afad76961c8a1570dbe06
nl79 <nl79@njit.edu>
2015-01-23 21:07:19 -0500

    updated users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,12 +1,15 @@
 USE nl79;
-
+drop table users; 
 CREATE TABLE users (
-user_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(20) NOT NULL,
 last_name VARCHAR(40) NOT NULL,
-email VARCHAR(60) NOT NULL,
+email VARCHAR(80) NOT NULL,
 pass CHAR(40) NOT NULL,
-active char(32) null default null, 
+user_level TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+active CHAR(32),
 registration_date DATETIME NOT NULL,
-PRIMARY KEY (user_id)
+PRIMARY KEY (user_id),
+UNIQUE KEY (email),
+INDEX login (email, pass)
 );
